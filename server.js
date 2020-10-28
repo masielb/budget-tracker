c24b855506b8d954240cee295ba3cd3e3b87e1a4
Masiel Bautista <masiel5@users.noreply.github.com>
2020-10-28 19:49:06 -0400

    initial files

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(logger("dev"));
+
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
